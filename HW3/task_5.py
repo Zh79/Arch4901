3aa122fc5be7dd9a5af09931810316540e169668
ZH <zharov.2010@yandex.ru>
2023-11-12 19:34:53 +0300

    dz

---
+++ HW3/task_5.py
@@ -0,0 +1,26 @@
+
+from abc import ABC, abstractmethod
+
+
+class Engine(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Car:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def start(self):
+        self.engine.start()
+
+
+class PetrolEngine(Engine):
+    def start(self):
+        print("Запуск бензинового двигателя...")
+
+
+class DieselEngine(Engine):
+    def start(self):
+        print("Запуск дизельного двигателя...")
